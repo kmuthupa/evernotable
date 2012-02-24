f77b64513e0ed28a2d4844a6e8c9af0a11118bc3
Karthik Muthupalaniappan <karthik.m@imaginea.com>
2012-02-24 12:59:24 +0530

    some tests for command

--- spec/command_spec.rb
+++ spec/command_spec.rb
@@ -2,6 +2,10 @@ require 'spec_helper'
 require 'evernotable/command'
 
 describe Evernotable::Command do
+  before(:all) do
+    Evernotable::Command.load
+  end
+  
   it 'should raise command failed error' do
     begin
       raise Evernotable::Command::CommandFailed, 'testing commandfailed'
@@ -12,9 +16,18 @@ describe Evernotable::Command do
   end
   
   it 'should load all the commands successfully' do
-    Evernotable::Command.load
     Evernotable::Command::Base.should_not be_nil
     Evernotable::Command::Auth.should_not be_nil
     Evernotable::Command::Task.should_not be_nil
+  end
+  
+  it 'should build the list of commands available' do
+    Evernotable::Command.commands.should == ['auth', 'base', 'task']
+  end
+  
+  it 'should test valid' do
+    Evernotable::Command.valid?('auth').should be_true
+    Evernotable::Command.valid?('task').should be_true
+    Evernotable::Command.valid?('wrong-one').should be_false
   end
 end
